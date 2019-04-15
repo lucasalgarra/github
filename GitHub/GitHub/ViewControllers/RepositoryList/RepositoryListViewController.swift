a79c4396d63116d475963fe5948ba1baf58f95ba
Lucas Algarra <lucas.algarra@singu.com.br>
2019-04-15 13:09:02 -0300

    Repository list created

---
+++ GitHub/GitHub/ViewControllers/RepositoryList/RepositoryListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RepositoryListViewController.swift
+//  GitHub
+//
+//  Created by Lucas Algarra on 15/04/19.
+//  Copyright © 2019 João Lucas Algarra. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryListViewController: UIViewController {
+    
+}
+
+//-----------------------------------------------------------------------------
+// MARK: - View lifecycle
+//-----------------------------------------------------------------------------
+
+extension RepositoryListViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
